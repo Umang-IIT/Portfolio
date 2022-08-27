7ea2c194b076194c1855e103831f3d798c78eca3
Umang-IIT <73515380+Umang-IIT@users.noreply.github.com>
2022-08-27 15:02:08 +0530

    changed homepage alignment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className='main'>
-        <InfoCard />
+        <InfoCard name = "Umang Singla" />
       </div>
     </div>
   );
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -4,17 +4,17 @@ import Linkedin from '../logo/linkedin-brands.svg';
 import Instagram from '../logo/instagram-brands.svg';
 import Codeforces from '../logo/codeforces-svgrepo-com.svg';
 
-const InfoCard = () => {
+const InfoCard = (prop) => {
   return (
     <div className="info">
-      <div className="name">Umang Singla</div>
+      <div className="name">{prop.name}</div>
       <div className="title">Full Stack Developer</div>
       <ul className="icons">
-          <li><a href='https://github.com/umang-singla'><i class="fa-brands fa-github"></i></a></li>
-          <li><a href='https://www.facebook.com/singlaumang/'><i class="fa-brands fa-facebook"></i></a></li>
-          <li><a href='https://www.instagram.com/umang__singla/'><i class="fa-brands fa-instagram"></i></a></li>
-          <li><a href='https://www.linkedin.com/in/umang-singla/'><i class="fa-brands fa-linkedin"></i></a></li>
-          <li><a href='https://codeforces.com/profile/umangsingla617'><i class="fa-solid fa-chart-simple fa-flip-horizontal"></i></a></li>
+          <li><a href='https://github.com/umang-singla'><i class="fa-brands fa-github icon"></i></a></li>
+          <li><a href='https://www.facebook.com/singlaumang/'><i class="fa-brands fa-facebook icon"></i></a></li>
+          <li><a href='https://www.instagram.com/umang__singla/'><i class="fa-brands fa-instagram icon"></i></a></li>
+          <li><a href='https://www.linkedin.com/in/umang-singla/'><i class="fa-brands fa-linkedin icon"></i></a></li>
+          <li><a href='https://codeforces.com/profile/umangsingla617'><i class="fa-solid fa-chart-simple icon fa-flip-horizontal"></i></a></li>
       </ul>
     </div>
   );
